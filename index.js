913b1873c165d84dc150986862e0e789b8d8e0ea
Njalale <fnjalale@gmail.com>
2023-03-18 22:47:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+console.log(writeCards(names, "graduation"));
+
+function countDown(){
+    let countdown = 10;
+    while (countdown > -1){
+        console.log(countdown--);
+    }
+}
